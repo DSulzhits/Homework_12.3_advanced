60a8ef8c1dce5651e42579a05003c42a26ebdf68
DSulzhits <dsulzhits@gmail.com>
2023-02-09 23:24:44 +0300

    Добавил фикстуры, доработал тесты

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -4,20 +4,14 @@ from utils import dicts
 
 @pytest.fixture
 def data():
-    return {"vcs": "mercurial"}
-
-
-@pytest.fixture
-def data_1():
-    return {}
+    return {"vcs": "mercurial"}, {}
 
 
 def test_key_value(data):
-    assert dicts.get_val(data, "vcs") == data("vcs")
-    assert dicts.get_val(data, "vcs", "git") == data("vcs")
-
-
-def test_key_value(data_1):
-    assert dicts.get_val(data_1, "vcs", "git") == "git"
-    assert dicts.get_val(data_1, "vcs", "bazaar") == "bazaar"
-    assert dicts.get_val(data_1, "") == "git"
+    test_data_1, test_data_2 = data
+    assert dicts.get_val(test_data_1, "vcs") == "mercurial"
+    assert dicts.get_val(test_data_1, "vcs", "git") == "mercurial"
+    assert dicts.get_val(test_data_1, "", "git") == "git"
+    assert dicts.get_val(test_data_2, "vcs", "git") == "git"
+    assert dicts.get_val(test_data_2, "vcs", "bazaar") == "bazaar"
+    assert dicts.get_val(test_data_2, "") == "git"
