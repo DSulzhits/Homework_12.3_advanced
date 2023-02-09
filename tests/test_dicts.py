33e2025822837c1855f735622876b3775ce40447
DSulzhits <dsulzhits@gmail.com>
2023-02-09 21:27:56 +0300

    add tests

---
+++ tests/test_dicts.py
@@ -0,0 +1,10 @@
+from utils import dicts
+
+
+def test_key_value():
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "", "git") == "git"
+    assert dicts.get_val({}, "vcs", "git") == "git"
+    assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
+    assert dicts.get_val({}, "", "git") == "git"
